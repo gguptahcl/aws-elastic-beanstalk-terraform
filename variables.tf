f54d70555c7f8b4880dd238ca9ac1110bb4a27a2
Gaurav gupta <g.gupta@hcl.com>
2018-09-19 20:01:30 -0700

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "profile" {
+  description = "Name of your profile inside ~/.aws/credentials"
+  default="default" 
+}
+
+variable "application_name" {
+  description = "Name of your application"
+}
+
+variable "application_description" {
+  description = "Sample application based on Elastic Beanstalk & Docker"
+}
+
+variable "application_environment" {
+  description = "Deployment stage e.g. 'staging', 'production', 'test', 'integration'"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  description = "Defines where your app should be deployed"
+}
+
+variable "access_key" {}
+
+variable "secret_key" {}
